774e720f5ec042f770088bb5591b6a612e2fd42d
Shubham Singh <shubhamsingh147@gmail.com>
2023-11-27 01:20:17 +0530

    creating empty middleware directory to handle authenticated routers

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -3,8 +3,11 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	controller "golang-jwt-artica/controllers"
+	middleware "golang-jwt-artica/middleware"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("users", controller.GetUsers())
+	incomingRoutes.GET("users/:user_id", controller.GetUser())
 }
