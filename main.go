1e2c941a9a78e48a87bba616e193cca9b3de166f
Shubham Singh <shubhamsingh147@gmail.com>
2023-11-27 01:07:49 +0530

    adding routers and sample APIs

--- main.go
+++ main.go
@@ -14,5 +14,18 @@ func main(){
     }
 
     router := gin.New()
+    router.Use(gin.Logger())
 
+    routes.AuthRoutes(router)
+    routes.UserRoutes(router)
+
+    router.GET("/api-1", func(c *gin.Context) {
+        c.JSON(200, gin.H{"success" : "Access Granted for API-1"})
+    })
+
+    router.GET("/api-2", func(c *gin.Context) {
+        c.JSON(200, gin.H{"success" : "Access Granted for API-2"})
+    })
+
+    router.RUN(":" + port)
 }
