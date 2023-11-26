4968b1f5163c3f439dd59df439401096066e3cb6
Shubham Singh <shubhamsingh147@gmail.com>
2023-11-27 01:54:31 +0530

    adding authHelper for userID matching and changing get param to id from user_id

---
+++ helpers/authHelper.go
@@ -0,0 +1,17 @@
+package helper
+
+import(
+    "errors"
+    "github.com/gin-gonic/gin"
+)
+
+func MatchUserTypeToUid(c *gin.Context, userId string) err error {
+    userType := c.GetString("user_type")
+    uid := c.GetString("uid")
+    if userType == "USER" & uid != userId {
+        err = errors.New("Unauthorized to access this resource")
+        return err
+    }
+    err = CheckUserType(c, userType)
+    return err
+}
