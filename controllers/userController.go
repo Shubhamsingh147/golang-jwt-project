a6e68f2eda8087c0d0fe2c5ab31344ddab9a42ea
Shubham Singh <shubhamsingh147@gmail.com>
2023-11-27 01:52:49 +0530

    adding controller code for GetUser route and helper function for it

--- controllers/userController.go
+++ controllers/userController.go
@@ -3,11 +3,32 @@ package controllers
 import(
     "golang-jwt-artica/database"
     "fmt"
+    "context"
+    "github.com/gin-gonic/gin"
+    "github.com/go-playground/validator/v10"
+    helper "golang-jwt-artica/helpers"
+    "log"
+    "strconv"
+    "net/http"
+    "time"
+
+
+    "golang-jwt-artica/models"
+    "golang.org/x/crypto/bcrypt"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, )
-
+var validate = validator.New()
 
 func GetUsers()
-func GetUser()
+func GetUser() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        userId := c.Param("id")
+
+        if err:= helper.MatchUserTypeToUid(c, userId); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{ "error" : err.Error() })
+            return
+        }
+    }
+}
 
