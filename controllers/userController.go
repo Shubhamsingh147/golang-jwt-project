24f54250c78f935fc0640a773b3733bfbaf6dda4
Shubham Singh <shubhamsingh147@gmail.com>
2023-11-27 01:41:37 +0530

    adding colelction with basic functionality

---
+++ controllers/userController.go
@@ -0,0 +1,13 @@
+package controllers
+
+import(
+    "golang-jwt-artica/database"
+    "fmt"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, )
+
+
+func GetUsers()
+func GetUser()
+
