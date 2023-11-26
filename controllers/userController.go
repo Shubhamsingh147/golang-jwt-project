ffe171427ba3253ba651455f9b705c9f674ed6cd
Shubham Singh <shubhamsingh147@gmail.com>
2023-11-27 02:27:04 +0530

    completed signUp API

--- controllers/userController.go
+++ controllers/userController.go
@@ -11,16 +11,18 @@ import(
     "strconv"
     "net/http"
     "time"
-
-
     "golang-jwt-artica/models"
     "golang.org/x/crypto/bcrypt"
+    "go.mongodb.org/mongodb-driver/bson"
+    "go.mongodb.org/mongodb-driver/bson/primitive"
+    "go.mongodb.org/mongodb-driver/mongo"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, )
 var validate = validator.New()
 
 func GetUsers()
+
 func GetUser() gin.HandlerFunc {
     return func(c *gin.Context) {
         userId := c.Param("id")
@@ -40,6 +42,60 @@ func GetUser() gin.HandlerFunc {
             return err
         }
         c.JSON(http.StatusOk, user)
+    }
+
+    func SignUp() gin.HandlerFunc {
+        return func(c *gin.Context){
+            var ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+            var user models.User
+
+            if err := c.BindJSON(&user); err != nil {
+                c.JSON(http.StatusBadRequest, gin.H{ "error" : err.Error() })
+                return
+            }
+
+            validationErr := validate.Struct(user)
+            if validationErr != nil {
+                c.JSON(http.StatusBadRequest, gin.H{ "error" : validationErr.Error() })
+                return
+            }
+
+            count, err := userCollection.CountDocuments(ctx, bson.M{"email" : user.Email})
+            defer cancel()
+            if err != nil {
+                log.panic(err)
+                c.JSON(http.StatusInternalServerError, gin.H{"error": "error while checking existing emails"})
+            }
+
+            count, err := userCollection.CountDocuments(ctx, bson.M{"phone" : user.Phone})
+            defer cancel()
+            if err != nil {
+                log.panic(err)
+                c.JSON(http.StatusInternalServerError, gin.H{"error": "error while checking existing phones"})
+            }
+
+            if count > 0 {
+                c.JSON(http.StatusInternalServerError, gin.H{"error": "userId with email or phone already exists"})
+            }
+
+            user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+            user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+            user.ID = primitive.NewObjectID()
+            user.UserId, _ = user.ID.Hex()
+            token, refreshToken, _ := helper.GenerateAllTokens(*user.Email, *user.FirstName, *user.LastName, *user.UserType, *&user.UserId)
+            user.Token = &token
+            user.RefreshToken = &refreshToken
+
+            resultInsertionNumber, insertErr := userCollection.InsertOne(ctx, user)
+            if insertErr != nil {
+                msg := fmt.Sprintf("user item was not created : %s", insertErr)
+                c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+                return
+            }
+            defer cancel()
+            c.JSON(http.StatusOk, resultInsertionNumber)
+        }
+
     }
 }
 
